ddf3950fd0b6692630481a7b2a8aade7298dacb2
eman.fatima <eman.fatima@cppapk.onmicrosoft.com>
2026-06-03 11:03:11 +0500

    api changes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     # CORS
     frontend_origins: list[str] = [
         "http://localhost:5173",
-        "http://192.168.11.123:5173",
+        "http://192.168.11.111:5173",
     ]
 
     class Config:
